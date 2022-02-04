b157663705f24fac91df62e3346bc0bca4f0f545
titojosuer <47695314+titojosuer@users.noreply.github.com>
2022-02-04 09:50:52 -0600

    Add files via upload
    routes

--- routes/web.php
+++ routes/web.php
@@ -14,26 +14,55 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => 'auth'],function() {
+  
+  Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+  Route::resource('clientes', App\Http\Controllers\clientesController::class);
+
+  Route::resource('productos', App\Http\Controllers\productosController::class);
+
+  Route::resource('ventas', App\Http\Controllers\VentaController::class);
+
+  Route::resource('proveedores', App\Http\Controllers\proveedoresController::class);
+
+  Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
+
+  Route::resource('tipoUsuarios', App\Http\Controllers\tipo_usuariosController::class);
+
+  Route::resource('regimenTributario', App\Http\Controllers\RegimenTributarioController::class)->names('regimenTributario');
+
+  Route::resource('facturas', App\Http\Controllers\facturasController::class);
+
+  Route::resource('pedidos',App\Http\Controllers\PedidoController::class );
+
+  Route::resource('permissions',App\Http\Controllers\PermissionController::class );
+
+  Route::resource('roles',App\Http\Controllers\RoleController::class );
+
+  Route::resource('users',App\Http\Controllers\UserController::class );
+
+  Route::resource('laboratorios',App\Http\Controllers\LaboratorioController::class );
+
+  Route::resource('empresas',App\Http\Controllers\EmpresaController::class)->only(['index','update']);
+
+  Route::resource('impresora', App\Http\Controllers\ImpresoraController::class)->only(['index','update']);
 
 
-Route::resource('clientes', App\Http\Controllers\clientesController::class);
+  Route::get('delete/productos/{id}', 'App\Http\Controllers\productosController@delete')->name('productos.delete');
+  Route::get('cambiar_estado/productos/{producto}','App\Http\Controllers\productosController@cambiar_estado')->name('cambiar.estado.producto');
+  Route::get('cambiar_estado/pedidos/{pedido}','App\Http\Controllers\PedidoController@cambiar_estado')->name('cambiar.estado.pedido');
+  Route::get('cambiar_estado/ventas/{venta}','App\Http\Controllers\VentaController@cambiar_estado')->name('cambiar.estado.venta');
 
-Route::resource('productos', App\Http\Controllers\productosController::class);
+  Route::get('print/ventas/{venta}', 'App\Http\Controllers\VentaController@print')->name('print.venta');
+  Route::get('get_productos_by_barcode','App\Http\Controllers\productosController@get_productos_by_barcode')->name('get_productos_by_barcode');
 
-Route::resource('ventas', App\Http\Controllers\VentaController::class);
+  Route::get('get_productos_by_id','App\Http\Controllers\productosController@get_productos_by_id')->name('get_productos_by_id');
 
-Route::resource('proveedores', App\Http\Controllers\proveedoresController::class);
-
-Route::resource('categorias', App\Http\Controllers\CategoriaController::class);
-
-Route::resource('tipoUsuarios', App\Http\Controllers\tipo_usuariosController::class);
-
-Route::resource('facturas', App\Http\Controllers\facturasController::class);
-
-Route::resource('compras/pedidos',App\Http\Controllers\PedidoController::class );
+});
